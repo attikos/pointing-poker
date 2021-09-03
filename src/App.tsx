220328336faf3c2bb4b4008dfa97dd8e75b7a6e1
Bylkov Maksim <vuegit@gmail.com>
2021-09-04 02:28:01 +0400

    feat: add bootstrap v5.0

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Установленная версия Bootstrap: 5.1</h1>
+        <h2><a href="https://getbootstrap.com/docs/5.0/layout/grid/" target="_blank">Документация</a></h2>
+
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+
+// declare global {
+//   interface Window {
+//     api?: any;
+//     bootstrap: bootstrap,
+//   }
+// }
+
+window.bootstrap = bootstrap;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
