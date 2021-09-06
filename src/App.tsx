85b31e539d21224f4d23d53ad73679fb2524222a
Bylkov Maksim <vuegit@gmail.com>
2021-09-06 15:38:30 +0400

    feat: add scss and css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={s.app}>
+      <header className={s.appHeader}>
+        <img src={logo} className={s.appLogo} alt="logo" />
 
         <h1>Установленная версия Bootstrap: 5.1</h1>
-        <h2><a href="https://getbootstrap.com/docs/5.0/layout/grid/" target="_blank">Документация</a></h2>
+        <h2><a href="https://getbootstrap.com/docs/5.0/layout/grid/" target="_blank" rel="noreferrer">Документация</a></h2>
 
       </header>
     </div>
