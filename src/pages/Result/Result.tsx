c4bbf42594961f393abf943dc82eaa76be03911e
KsenNeko <anekokato@gmail.com>
2021-10-01 17:04:55 +0400

    fix: save file in csv format

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -8,6 +8,10 @@ import { RootState } from '../../store/store-redux';
 import s from './Results.module.scss';
 import cn from 'classnames';
 import { Link } from 'react-router-dom';
+import { array } from 'yup';
+import { split } from 'lodash';
+
+const NUMBER_OF_COLUMNS = 3;
 
 const Result = (): JSX.Element => {
   const issues = useSelector((state: RootState) => state.allData.issues);
@@ -22,6 +26,14 @@ const Result = (): JSX.Element => {
 
   removeStatusCurrent();
 
+  const issueForSave = (issueId: number) => {
+    const curIssue = issues.find((item) => item.id === issueId);
+    const link = curIssue?.link ? ', link: ' + curIssue?.link : '';
+    return (
+      'issue: ' + curIssue?.title + ', priority: ' + curIssue?.priority + link
+    );
+  };
+
   const listResults = () => {
     return (
       <div className={s.listResults}>
@@ -31,7 +43,7 @@ const Result = (): JSX.Element => {
             <StatisticCards
               idCurrentIssue={item.id}
               returnResult={(issueId: number, results: any) =>
-                (arrayResults['issueId: ' + issueId] = results)
+                (arrayResults[issueForSave(issueId)] = results)
               }
             />
           </div>
@@ -40,12 +52,38 @@ const Result = (): JSX.Element => {
     );
   };
 
+  const parsStr = (key: string) => {
+    return key.split(',');
+  };
+
+  const parsObj = (obj: any) => {
+    let str = '';
+    for (const key in obj) {
+      str = `${str} (${key}:${obj[key]}) `;
+    }
+    return str;
+  };
+
+  
+
   const downloadResults = () => {
-    const blob = new Blob([JSON.stringify(arrayResults, null, 2)], {
-      type: 'application/json',
+    const data: any = [
+      ['issue', 'priority', 'link', 'result(score:voices)', '\r\n'],
+    ];
+    for (const key in arrayResults) {
+      const arr = parsStr(key).map((item) => item.split(':')[1]);
+      if (arr.length !== NUMBER_OF_COLUMNS) arr.push('-');
+      data.push([
+        arr,
+        parsObj(arrayResults[key]),
+        '\r\n',
+      ]);
+    }
+    const blob = new Blob(data, {
+      type: 'text/csv;charset=utf-8',
     });
     const a = document.createElement('a');
-    a.download = 'result.txt';
+    a.download = 'result.csv';
     a.href = URL.createObjectURL(blob);
     a.click();
     URL.revokeObjectURL(a.href);
@@ -55,6 +93,12 @@ const Result = (): JSX.Element => {
     <div className={s.resultWrapper}>
       <div className={s.topic}>
         {issues.map((item: IIssue) => item.title.slice(0, 10)).join(', ')}
+      </div>
+      <div className={s.attention}>
+        ATTENTION! For view results in Excel: click the Data tab, then From
+        Text. Select the CSV file that has the data clustered into one column.
+        Select Delimited, then make sure the File Origin is Unicode UTF-8.
+        Select Comma. Finally, click Finish.
       </div>
       <div className={s.controlBtn}>
         <Link to='/'>
