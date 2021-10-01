e26c39025e587b7b131eec99f6ffb5ccdc092ba0
KsenNeko <anekokato@gmail.com>
2021-10-01 11:16:09 +0400

    add: check mark

--- src/components/IssueCard/IssueCard.tsx
+++ src/components/IssueCard/IssueCard.tsx
@@ -1,6 +1,6 @@
 import cn from 'classnames';
 import React from 'react';
-import { HiOutlineTrash } from 'react-icons/hi';
+import { HiCheck, HiOutlineTrash } from 'react-icons/hi';
 import { IIssue } from '../../interface';
 import s from './IssueCard.module.scss';
 
@@ -9,20 +9,26 @@ interface IProps {
   onSetIsCurrentIssue?: any;
   onDeleteIssue?: any;
 }
-const IssueCard = ({ issue, onSetIsCurrentIssue = () => {}, onDeleteIssue }: IProps): JSX.Element => {
+const IssueCard = ({
+  issue,
+  onSetIsCurrentIssue = () => {},
+  onDeleteIssue,
+}: IProps): JSX.Element => {
   return (
     <div
-      className={ cn( s.issuesCard, { [s.issuesCardCurrent] : issue.isCurrent }) }
+      className={cn(s.issuesCard, { [s.issuesCardCurrent]: issue.isCurrent })}
       onClick={() => onSetIsCurrentIssue()}
     >
       <div className={s.issuesInfo}>
         <div className={s.issuesInfoName}>{issue.title}</div>
         <div className={s.issuesPriority}>{issue.priority}</div>
       </div>
-
-      { onDeleteIssue ? (<div className={s.issuesDel} onClick={() => onDeleteIssue(issue.id)}>
-        <HiOutlineTrash className={s.issuesDelIcon} />
-      </div>) : null }
+      {issue.status === 'finished' ? <HiCheck /> : null}
+      {onDeleteIssue ? (
+        <div className={s.issuesDel} onClick={() => onDeleteIssue(issue.id)}>
+          <HiOutlineTrash className={s.issuesDelIcon} />
+        </div>
+      ) : null}
     </div>
   );
 };
