6cce380ae7d8b8edd1150266d16225e33ff9fccd
Bylkov Maksim <vuegit@gmail.com>
2021-10-06 13:20:55 +0400

    fix: add correct server url for CORS

--- src/env/production.env.js
+++ src/env/production.env.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://mighty-savannah-90439.herokuapp.com';
-export const API_URL_WEBSOCKET = 'wss://mighty-savannah-90439.herokuapp.com';
+export const API_URL = 'https://attikos-pointing-poker-server.herokuapp.com';
+export const API_URL_WEBSOCKET = 'wss://attikos-pointing-poker-server.herokuapp.com';
